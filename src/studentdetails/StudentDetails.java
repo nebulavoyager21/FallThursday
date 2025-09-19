0c96d161c4d3d8875563074940f7396005ef41a1
Abdul Rahman Pennino <72480260+nebulavoyager21@users.noreply.github.com>
2025-09-19 03:59:03 -0400

    Update StudentDetails.java
    I added the date for today.

--- src/studentdetails/StudentDetails.java
+++ src/studentdetails/StudentDetails.java
@@ -14,6 +14,7 @@ public class StudentDetails {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        // Friday, September 19, 2025
        // print sid, name of 3 students
        
        Student s1 = new Student();
